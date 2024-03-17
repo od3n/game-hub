8fc31c254fe88b6a65f47433313ed4153e77d0bf
Salahuddin Hairai <mr.od3n@gmail.com>
2024-03-18 02:41:28 +0800

    Removing duplicate interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
 import { Genre } from './hooks/useGenres';
-import { Platform } from './hooks/useGames';
+import { Platform } from './hooks/usePlatforms';
 import { useState } from 'react';
 import PlatformSelector from './components/PlatformSelector';
 import SortSelector from './components/SortSelector';
